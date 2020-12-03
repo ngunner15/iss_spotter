3aa43c259405fa625e15359fbe184f686e22b3c8
Nisarg <nisargvv@gmail.com>
2020-12-03 21:01:20 +0000

    Created fetchCoordsByIP and fetchISSFlyOverTimes functions

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('101.5.40.1', (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Coords:' , coords);
+// });
+
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It did not work!", error);
     return;
+  } else {
+    console.log("It worked! Returned array: ", data);
   }
-
-  console.log('It worked! Returned IP:' , ip);
 });
--- iss.js
+++ iss.js
@@ -17,4 +17,35 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`,(error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const lat = JSON.parse(body).latitude;
+    const long = JSON.parse(body).longitude;
+    callback(null, {latitude: lat, longitude: long});
+  });
+};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`,(error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const dataReturn = JSON.parse(body).response;
+    callback(null, dataReturn);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
